486eb2036d0ccf6f32057b074244b0ed0d26a298
Lucien <lucien.neuhoff@protonmail.com>
2024-03-02 13:17:41 +0100

    feat: basic level

--- src/levels/dev_level.py
+++ src/levels/dev_level.py
@@ -3,34 +3,21 @@ from .level import Level
 from ..settings import *
 from ..camera import Camera
 from ..player import Player
-from pygame.event import Event as Event
 
 
 class DevLevel(Level):
     def __init__(self, game) -> None:
+        super().__init__(game, "dev")
         from ..main import Game
 
         self.game: Game = game
 
         self.camera = Camera()
 
-        self.player = Player(self, display_width / 2, display_height / 2)
+        self.player = Player(self, self.spawn_point.x, self.spawn_point.y)
         self.camera.follow = self.player.rect
 
-        self.tiles = [
-            Tile(0, 4),
-            Tile(3, 5),
-            Tile(2, 4),
-            Tile(4, 5),
-            Tile(5, 5),
-            Tile(1, 3),
-            Tile(6, 3),
-            Tile(6, 5),
-            Tile(7, 3),
-            Tile(7, 4),
-            Tile(7, 5),
-        ]  # , Tile(1, 4), Tile(2, 4), Tile(5, 2)
-
+        
     def handle_events(self, events: list[pg.Event]):
         self.player.handle_events(events)
 
--- src/levels/level.py
+++ src/levels/level.py
@@ -1,11 +1,28 @@
+from PIL import Image
+from ..tile import Tile
 from ..settings import *
 
 
 class Level:
-    def __init__(self, game) -> None:
+    def __init__(self, game, name: str) -> None:
         from ..main import Game
 
         self.game: Game = game
+        self.name = name
+
+        self.tiles: list[Tile] = []
+        self.spawn_point = vector(0, 0)
+        self.load(f"assets/levels/{self.name}.png")
+
+    def load(self, path: str):
+        img = Image.open(path)
+        for y in range(img.height):
+            for x in range(img.width):
+                pixel = img.getpixel((x, y))
+                if pixel == 0:
+                    self.tiles.append(Tile(x, y))
+                if pixel == 3:
+                    self.spawn_point = vector(x * 32, y * 32)
 
     def handle_events(self, events: list[pg.Event]):
         pass
