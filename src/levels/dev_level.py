542ae2bf0fbd17c6760e81b75fa9ee04d605447d
Lucien <lucien.neuhoff@protonmail.com>
2024-03-02 12:37:32 +0100

    added tiles

--- src/levels/dev_level.py
+++ src/levels/dev_level.py
@@ -24,6 +24,10 @@ class DevLevel(Level):
             Tile(5, 5),
             Tile(1, 3),
             Tile(6, 3),
+            Tile(6, 5),
+            Tile(7, 3),
+            Tile(7, 4),
+            Tile(7, 5),
         ]  # , Tile(1, 4), Tile(2, 4), Tile(5, 2)
 
     def handle_events(self, events: list[pg.Event]):
