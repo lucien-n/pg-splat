a06571ee8824731584d807268e16df125a5e3898
Lucien <lucien.neuhoff@protonmail.com>
2024-04-20 15:57:57 +0200

    feat: :sparkles: debug line separator

--- src/hud.py
+++ src/hud.py
@@ -1,7 +1,7 @@
 import numpy as np
 from .settings import *
 import pygame.freetype as pgft
-import typing
+import random
 
 
 class Hud:
@@ -44,7 +44,9 @@ class Hud:
             (218, 113, 127),
         )
 
+        self.debug_separator()
         # draw movement keybinds
+        self.debug("bindc_movements", "MOVEMENTS")
         for bind in settings["keybinds"]["movements"]:
             key = settings["keybinds"]["movements"][bind]
             self.debug(
@@ -52,36 +54,54 @@ class Hud:
                 f"{bind.upper()}: {"SPACE" if key == " " else key.upper()}",
             )
 
+        self.debug_separator()
+        # draw misc keybinds
+        self.debug("bindc_misc", "MISC", bg_color=(83, 191, 92))
+        for bind in settings["keybinds"]["misc"]:
+            key = settings["keybinds"]["misc"][bind]
+            self.debug(
+                f"bind_{bind}",
+                f"{bind.upper()}: {"SPACE" if key == " " else key.upper()}",
+                bg_color=(83, 191, 92),
+            )
+
         self.redraw()
 
     def redraw(self):
         self.rendered_lines.clear()
-        for rendered_line in self.debug_lines.values():
-            value = rendered_line.get("value")
-            label = rendered_line.get("label")
-            unit = rendered_line.get("unit")
-            fg_color = rendered_line.get("fg_color", (255, 255, 255))
-            bg_color = rendered_line.get("bg_color", (253, 187, 109))
 
-            rendered_line = self.render_font(
-                f'{label + " " if label else ""}{value}{unit if unit else ""}',
-                20,
-                fg_color,
-                bg_color,
-                5,
-            )
+        for debug_line in self.debug_lines.values():
+            if debug_line is None:
+                rendered_line = None
+            else:
+                value = debug_line.get("value")
+                label = debug_line.get("label")
+                unit = debug_line.get("unit")
+                fg_color = debug_line.get("fg_color", (255, 255, 255))
+                bg_color = debug_line.get("bg_color", (253, 187, 109))
+
+                rendered_line = self.render_font(
+                    f'{label + " " if label else ""}{value}{unit if unit else ""}',
+                    20,
+                    fg_color,
+                    bg_color,
+                    5,
+                )
             self.rendered_lines.append(rendered_line)
 
     def draw(self, surface: pg.Surface, *args):
         h = 0
         for rendered_line in self.rendered_lines:
-            surface.blit(rendered_line, (0, h))
-            h += rendered_line.get_height()
+            if rendered_line is None:
+                h += 20
+            else:
+                surface.blit(rendered_line, (0, h))
+                h += rendered_line.get_height()
 
     def debug(
         self,
         key: str,
-        value: typing.Any,
+        value: str | int | float | object,
         label: str | None = None,
         bg_color: pg.Color | None = None,
         fg_color: pg.Color | None = None,
@@ -99,6 +119,9 @@ class Hud:
             self.debug_lines[key]["bg_color"] = bg_color
         if fg_color:
             self.debug_lines[key]["fg_color"] = fg_color
+
+    def debug_separator(self):
+        self.debug_lines[f"sep_{random.randint(100, 999)}"] = None
 
     def render_font(
         self,
--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,10 @@ from pygame.math import Vector2 as vector
 settings = {
     "display": {"target_fps": 165, "width": 1280, "height": 720},
     "camera": {"smoothness": 180},
-    "keybinds": {"movements": {"jump": " ", "right": "d", "left": "a"}},
+    "keybinds": {
+        "movements": {"jump": " ", "right": "d", "left": "a"},
+        "misc": {"debug": "f3"},
+    },
 }
 
 display_size = display_width, display_height = 320, 180
